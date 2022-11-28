651fdf394eaf63a38f623dff47937e5b1f1e4181
Robert <robertschauss91@gmail.com>
2022-11-28 01:43:06 -0800

    Still trying to get loop to display results. cleaned up code and realized starter code goes at the end.

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var char = ("abcdefghijklmnopqrstuvwxyz!@#$%&");
 var char2 = ("ABCDEFGHIJKLMONPQRSTUVWXY");
 var char3 = ("1234567890");
 var char4 = ("!@#$%&");
-
 var passwordLength = "";
 var charPrompt
 var char2prompt
@@ -17,29 +16,29 @@ var char3Prompt
 var char4Prompt
 
 function generatePassword() {
-    var passwordLength = parseInt(prompt("Password Lenght : Must be between 8 and 128 character"));
-    while ( passwordLength > 8 || passwordLength < 128) {
+    var passwordLength = parseInt(prompt("Password must be between 8 and 128 character"));
+    while ( passwordLength <= 8 || passwordLength >= 128) {
         alert("keep password length between 8-128 characters");
         var passwordLength = parseInt(prompt("Password Lenght must be between 8 and 128 character"))
     }
-    alert("good choice, you password is " + passwordLength + "characters")
+    alert("good choice, you password is " + passwordLength +  "characters")
+
+var charPrompt = confirm("use lowercase letter?");
+var char2prompt = confirm("use capital letters?");
+var char3Prompt = confrim("use numbers?");
+var char4Prompt = confirm("use special letters?");
 
 }
-var generateBtn = document.querySelector("#generate");
-
 
 console.log ();
 
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+    
+      passwordText.value = password;
+}
+
 generateBtn.addEventListener("click", generatePassword);
-
-// var generateBtn = document.querySelector("#generate");
-
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// generateBtn.addEventListener("click", writePassword);
